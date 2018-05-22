9e7d752fab81012c4a851ca03144e1c8b959b18c
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-22 10:53:17 -0700

    from class

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-/* Code goes here */
+const URL = 'https://petdibs.herokuapp.com/pets';
 
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+}
+
+const loadPets = () => {
+  const petList = $('#pet-list');
+  petList.empty();
+
+  reportStatus('Loading Pets!  Please Wait...');
+
+  // get the thing
+  axios.get(URL)
+    .then((response) => {
+      console.log('inside the .then');
+      response.data.forEach((animal) => {
+        console.log(animal);
+        petList.append(`<li>${animal.name}</li>`);
+      });
+      reportStatus('Pets Loaded!');
+    })
+    .catch((error) => {
+      console.log(error);
+      reportStatus(`Error: ${error.message }`);
+    });
+
+  console.log('This is after .get');
+}
+
+$(document).ready(() => {
+  $('#load').click(loadPets);
+})
