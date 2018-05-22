4385d07908f62b4b832e08c6670b42af2e384874
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-22 13:51:51 -0700

    added seven wonders

--- index.js
+++ index.js
@@ -1,33 +1,78 @@
-const URL = 'https://petdibs.herokuapp.com/pets';
+const places = [
+  "Great Pyramid of Giza",
+  "Hanging Gardens of Babylon",
+  "Colossus of Rhodes",
+  "Pharos of Alexandria",
+  "Statue of Zeus at Olympia",
+  "Temple of Artemis",
+  "Mausoleum at Halicarnassus"
+];
 
-const reportStatus = (message) => {
-  $('#status-message').html(message);
-}
+const buildURL = function (place) {
+  let key = 'AIzaSyAI0-N0rLVgoKPsN-2pzUkRbQSticlDw8E';
+  return `https://maps.googleapis.com/maps/api/geocode/json?address=${place}&key=${key}`;
+};
 
-const loadPets = () => {
-  const petList = $('#pet-list');
-  petList.empty();
+const getLocationFromResponse = (response) => {
+  return response['data']['results'][0]['geometry']['location'];
+};
 
-  reportStatus('Loading Pets!  Please Wait...');
+const reportStatus = (message) => { $('#status-message').html(message); };
 
-  // get the thing
-  axios.get(URL)
-    .then((response) => {
-      console.log('inside the .then');
-      response.data.forEach((animal) => {
-        console.log(animal);
-        petList.append(`<li>${animal.name}</li>`);
+const loadWonders = () => {
+  const wondersList = $('#wonder-list');
+  wondersList.empty();
+
+  reportStatus('Loading Wonders!  Please Wait...');
+  places.forEach((place) => {
+    axios.get(buildURL(place))
+      .then((response) => {
+        // console.log(response);
+        let location = getLocationFromResponse(response);
+        wondersList.append(`<li>${place} - ${location['lat']}, ${location['lng']} </li>`);
+        reportStatus('Wonders Loaded!');
+      })
+      .catch((error) => {
+        console.log(error);
+        reportStatus(`Error: ${error.message }`);
       });
-      reportStatus('Pets Loaded!');
-    })
-    .catch((error) => {
-      console.log(error);
-      reportStatus(`Error: ${error.message }`);
-    });
-
-  console.log('This is after .get');
-}
+  });
+};
 
 $(document).ready(() => {
-  $('#load').click(loadPets);
-})
+  $('#load').click(loadWonders);
+});
+
+// const URL = 'https://petdibs.herokuapp.com/pets';
+//
+// const reportStatus = (message) => {
+//   $('#status-message').html(message);
+// }
+//
+// const loadPets = () => {
+//   const petList = $('#pet-list');
+//   petList.empty();
+//
+//   reportStatus('Loading Pets!  Please Wait...');
+//
+//   // get the thing
+//   axios.get(URL)
+//     .then((response) => {
+//       console.log('inside the .then');
+//       response.data.forEach((animal) => {
+//         console.log(animal);
+//         petList.append(`<li>${animal.name}</li>`);
+//       });
+//       reportStatus('Pets Loaded!');
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       reportStatus(`Error: ${error.message }`);
+//     });
+//
+//   console.log('This is after .get');
+// }
+//
+// $(document).ready(() => {
+//   $('#load').click(loadPets);
+// })
